9af42a2945f2b9393f2fb3527d39f82a5b21e880
Ekaterina Anikina <reityan97@mail.ru>
2022-07-14 20:17:06 +0300

    Добавлена 4-я задача

--- example/Program.cs
+++ example/Program.cs
@@ -67,4 +67,32 @@ void Ex_003()
         }
 }
 
-Ex_003();
+//Ex_003();
+
+void Ex_004()
+{
+    //Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+    Console.WriteLine ("Введите число: ");
+    int number = Convert.ToInt32 (Console.ReadLine());
+    int i = 1;
+
+    while (i <= number)
+    {
+        int result = i % 2;
+        
+            if (result != 1)
+                {
+                    Console.WriteLine("четное " + i);
+                    i++;
+                }
+                else
+                {
+                    i++;
+                }
+        }
+        
+
+}
+
+Ex_004();
